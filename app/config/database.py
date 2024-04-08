2b96a2b9538bf14f773a8b71096a27a9cb7193eb
jesusran06 <jesusran06@gmail.com>
2024-04-08 18:56:54 -0400

    correcion em clave bd

--- app/config/database.py
+++ app/config/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'postgresql://yhnygykw:rVUfMTQlkQu0TkRIqhGiprIUjm3Dqr6q@kesavan.db.elephantsql.com:5432/yhnygykw'
+URL_DATABASE = 'postgresql://yhnygykw:ytoXCW2YvMhmd9_fegsr3qm0bHuJnV1A@kesavan.db.elephantsql.com:5432/yhnygykw'
 
 engine = create_engine(URL_DATABASE)
 
